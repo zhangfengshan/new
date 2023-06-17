ad76fe728ad1e36f418f9476c2a3564bd8a57c87
axx100b <103262848+zhangfengshan@users.noreply.github.com>
2023-06-17 23:30:36 +0800

    更新 abcdefghij.lua

--- abcdefghij.lua
+++ abcdefghij.lua
@@ -1,1 +1,23 @@
-loadstring(game:HttpGet('https://raw.githubusercontent.com/lxnnydev/DeltaAndroid/main/new_main_ui.lua', true))()
+function de()
+local function callback(Text)
+        if Text == "确定" then
+            loadstring(game:HttpGet('https://raw.githubusercontent.com/lxnnydev/DeltaAndroid/main/new_main_ui.lua', true))()
+        elseif Text == "取消" then
+
+        end
+    end
+
+    local NotificationBindable = Instance.new("BindableFunction")
+    NotificationBindable.OnInvoke = callback
+    game.StarterGui:SetCore("SendNotification", {
+        Title = "微山工作室",
+        Text = "是否加载delta面板",
+        Icon = "",
+        Duration = 20,
+        Button1 = "确定",
+        Button2 = "取消",
+        Callback = NotificationBindable
+    })
+
+end
+function de()
