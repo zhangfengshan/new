d9ae77ece51d0cbd2e74fe18b620e41d59f57ac6
axx100b <103262848+zhangfengshan@users.noreply.github.com>
2023-06-18 07:30:30 +0800

    更新 abcdefghij.lua

--- abcdefghij.lua
+++ abcdefghij.lua
@@ -12,27 +12,7 @@ local function callback(Text)
         Title = "微山工作室",
         Text = "是否加载delta面板",
         Icon = "",
-        Duration = 5,
-        Button1 = "确定",
-        Button2 = "取消",
-        Callback = NotificationBindable
-    })
-wait(3)
-local function callback(Text)
-        if Text == "确定" then
-            loadstring(game:HttpGet('https://raw.githubusercontent.com/Maikderninja/Maikderninja/ce7ff8f285a3aeece56cd1b5615ac445e2a6faca/krnl%20mobile.lua', true))()
-        elseif Text == "取消" then
-
-        end
-    end
-
-    local NotificationBindable = Instance.new("BindableFunction")
-    NotificationBindable.OnInvoke = callback
-    game.StarterGui:SetCore("SendNotification", {
-        Title = "微山工作室",
-        Text = "是否加载arceus面板",
-        Icon = "",
-        Duration = 5,
+        Duration = 50,
         Button1 = "确定",
         Button2 = "取消",
         Callback = NotificationBindable
