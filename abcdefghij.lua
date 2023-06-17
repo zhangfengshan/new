3f0f945fcecc0ef8dc833dba53fdfcb47fddc3e3
axx100b <103262848+zhangfengshan@users.noreply.github.com>
2023-06-18 07:22:07 +0800

    更新 abcdefghij.lua

--- abcdefghij.lua
+++ abcdefghij.lua
@@ -1,1 +1,39 @@
-loadstring(game:HttpGet('https://raw.githubusercontent.com/lxnnydev/DeltaAndroid/main/new_main_ui.lua', true))()
+local function callback(Text)
+        if Text == "确定" then
+            loadstring(game:HttpGet('https://raw.githubusercontent.com/lxnnydev/DeltaAndroid/main/new_main_ui.lua', true))()
+        elseif Text == "取消" then
+
+        end
+    end
+
+    local NotificationBindable = Instance.new("BindableFunction")
+    NotificationBindable.OnInvoke = callback
+    game.StarterGui:SetCore("SendNotification", {
+        Title = "微山工作室",
+        Text = "是否加载delta面板",
+        Icon = "",
+        Duration = 5,
+        Button1 = "确定",
+        Button2 = "取消",
+        Callback = NotificationBindable
+    })
+wait(3)
+local function callback(Text)
+        if Text == "确定" then
+            loadstring(game:HttpGet('https://raw.githubusercontent.com/Maikderninja/Maikderninja/ce7ff8f285a3aeece56cd1b5615ac445e2a6faca/krnl%20mobile.lua', true))()
+        elseif Text == "取消" then
+
+        end
+    end
+
+    local NotificationBindable = Instance.new("BindableFunction")
+    NotificationBindable.OnInvoke = callback
+    game.StarterGui:SetCore("SendNotification", {
+        Title = "微山工作室",
+        Text = "是否加载arceus面板",
+        Icon = "",
+        Duration = 5,
+        Button1 = "确定",
+        Button2 = "取消",
+        Callback = NotificationBindable
+    })
