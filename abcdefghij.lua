5c142c71346530e2490bd20ded162c42b4dbe169
axx100b <103262848+zhangfengshan@users.noreply.github.com>
2023-06-18 16:12:32 +0800

    更新 abcdefghij.lua

--- abcdefghij.lua
+++ abcdefghij.lua
@@ -1,2 +1,38 @@
-loadstring(game:HttpGet('https://raw.githubusercontent.com/zhangfengshan/zhangfengshan/main/l%20loxe%20you', true))()
-print("OK")
+function callback(Text) 
+         if Text == "确定" then 
+             loadstring(game:HttpGet('https://pastebin.com/raw/ZBKY6xDJ', true))() 
+         elseif Text == "取消" then 
+         end 
+     end 
+  
+     NotificationBindable = Instance.new("BindableFunction") 
+     NotificationBindable.OnInvoke = callback 
+     game.StarterGui:SetCore("SendNotification", { 
+         Title = "微山工作室", 
+         Text = "是否加载delta面板", 
+         Icon = "", 
+         Duration = 50, 
+         Button1 = "确定", 
+         Button2 = "取消", 
+         Callback = NotificationBindable 
+     }) 
+     wait(3) 
+     function callback1(Text) 
+         if Text == "确定" then 
+             loadstring(game:HttpGet('https://raw.githubusercontent.com/zhangfengshan/new/main/qwer.lua', true))() 
+         elseif Text == "取消" then 
+         end 
+     end 
+  
+     NotificationBindable1 = Instance.new("BindableFunction") 
+     NotificationBindable1.OnInvoke = callback1 
+     game.StarterGui:SetCore("SendNotification", { 
+         Title = "微山工作室", 
+         Text = "是否加载arceus面板", 
+         Icon = "", 
+         Duration = 50, 
+         Button1 = "确定", 
+         Button2 = "取消", 
+         Callback = NotificationBindable1 
+     }) 
+    
